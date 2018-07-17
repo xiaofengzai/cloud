4a1a209fa592ed9085015f48fdc534c6de35ea3d
feng.wen <feng.wen@sao.so>
2018-07-17 16:32:37 +0800

    配置中心

--- eurekaserver/src/main/java/com/wen/EurekaServerApplication.java
+++ eurekaserver/src/main/java/com/wen/EurekaServerApplication.java
@@ -3,9 +3,11 @@ package com.wen;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableEurekaServer
 @SpringBootApplication
+@RestController
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
